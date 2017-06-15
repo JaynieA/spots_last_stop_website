1b394c9aa86be0e982238e9c5eae49d804b22e04
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2017-06-14 19:15:59 -0500

    add navbar styles, add jumbotron background to home

--- public/components/NavbarInstance.jsx
+++ public/components/NavbarInstance.jsx
@@ -15,7 +15,7 @@ const NavbarInstance = (props) => (
     <Navbar.Collapse>
       <Nav pullRight>
         <NavItem eventKey={1} href='/Home'>Home</NavItem>
-        <NavDropdown eventKey={2} title='Adopt' id='basic-nav-dropdown'>
+        <NavDropdown eventKey={2} title='Adopt' id='adopt-nav-dropdown'>
           <MenuItem eventKey={2.1}>Adoptable Dogs</MenuItem>
           <MenuItem eventKey={2.2}>Adoption Application</MenuItem>
           <MenuItem eventKey={2.3}>FAQ's & Policies</MenuItem>
@@ -23,17 +23,17 @@ const NavbarInstance = (props) => (
           <MenuItem eventKey={2.5}>Happy Tails</MenuItem>
           <MenuItem eventKey={2.6}>Update Us</MenuItem>
         </NavDropdown>
-        <NavDropdown eventKey={3} title='Foster' id='basic-nav-dropdown'>
+        <NavDropdown eventKey={3} title='Foster' id='foster-nav-dropdown'>
           <MenuItem eventKey={3.1}>Foster Application</MenuItem>
           <MenuItem eventKey={3.2}>FAQ's & Policies</MenuItem>
           <MenuItem eventKey={3.3}>Foster Update</MenuItem>
         </NavDropdown>
         <NavItem eventKey={4} href='#'>Events</NavItem>
-        <NavDropdown eventKey={5} title='Support' id='basic-nav-dropdown'>
+        <NavDropdown eventKey={5} title='Support' id='support-nav-dropdown'>
           <MenuItem eventKey={5.1}>Donate</MenuItem>
           <MenuItem eventKey={5.2}>Our Partners</MenuItem>
         </NavDropdown>
-        <NavDropdown eventKey={6} title='About' id='basic-nav-dropdown'>
+        <NavDropdown eventKey={6} title='About' id='about-nav-dropdown'>
           <MenuItem eventKey={6.1}>About SLS</MenuItem>
           <MenuItem eventKey={6.2}>Our Team</MenuItem>
         </NavDropdown>
--- public/containers/HomeContainer.jsx
+++ public/containers/HomeContainer.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import {Jumbotron} from 'react-bootstrap';
 
 class HomeContainer extends React.Component {
   render() {
     return (
       <div>
-        <h1>Home</h1>
+        <Jumbotron>
+          <h1>Home</h1>
+        </Jumbotron>
       </div>
     )
   }
